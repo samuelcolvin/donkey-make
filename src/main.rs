c42fb9b24c8b96b7a5aa062d163a8564652edf72
Samuel Colvin <s@muelcolvin.com>
2019-04-06 17:28:55 +0100

    err macro

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ extern crate serde_yaml;
 #[macro_use]
 extern crate serde_derive;
 
-use std::process;
 use std::fs::File;
 use std::path::Path;
+use std::process;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Person {
@@ -14,19 +14,26 @@ struct Person {
     phones: Vec<String>,
 }
 
+macro_rules! err {
+    ($msg:expr) => (
+        Err($msg)
+    );
+    ($fmt:expr, $($arg:expr),+) => (
+        Err(format!($fmt, $($arg),+))
+    );
+}
+
 fn load_file() -> Result<Person, String> {
-    let path = Path::new("donkey2.yaml");
+    let path = Path::new("donkey.yaml");
 
     let file = match File::open(&path) {
         Ok(t) => t,
-        Err(why) => {
-            return Err(format!("couldn't open {}, {}", path.display(), why));
-        }
+        Err(e) => return err!("couldn't open {}, {}", path.display(), e),
     };
 
     let p: Person = match serde_yaml::from_reader(file) {
         Ok(t) => t,
-        Err(why) => return Err(format!("YAML error: {}", why)),
+        Err(e) => return err!("YAML error: {}", e),
     };
     Ok(p)
 }
@@ -35,7 +42,7 @@ fn main() {
     let p = match load_file() {
         Ok(t) => t,
         Err(s) => {
-            println!("Error Loading file: {}", s);
+            eprintln!("Error Loading file:\n  {}", s);
             process::exit(1);
         }
     };
