9dc3ad1c08b3056c04421cd9651d2c880c71c4b1
Samuel Colvin <s@muelcolvin.com>
2019-04-26 22:27:14 +0100

    fix python test fixtures

--- tests/test_cargo.py
+++ tests/test_cargo.py
@@ -9,14 +9,12 @@ THIS_DIR = Path(__file__).parent
 
 
 @pytest.mark.skipif('--cov' not in sys.argv, reason='only run for coverage')
-def test_cargo_coverage(coverage_ex, request):
+def test_cargo_coverage(coverage_ex, coverage_dir):
     """
-    Run cargo tests with coverage enabled.
-
-    Must be run last for coverage to work properly
+    Run cargo tests with coverage enabled
     """
     cov_dir = THIS_DIR / '../.coverage/cargo_test'
-    cov_dir.mkdir(parents=True)
+    cov_dir.mkdir()
 
     target = os.getenv('TARGET')
     debug_dir = (THIS_DIR / '../target{}/debug/'.format('/' + target if target else '')).resolve()
